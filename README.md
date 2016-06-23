a627c8eff1c34b515db7f0f79b90edacb784862e
Enes Erdogan <enes@erdogan.co>
2016-06-23 09:49:04 +0300

    :tada: Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# iftar
-İftar Vakti Cli Tool
+<p align="center">
+<a href="https://dribbble.com/shots/1961669-Pin-Istanbul-City"><img src="https://d13yacurqjgara.cloudfront.net/users/43340/screenshots/1961669/pin-istanbul-city.png" width="300"></a>
+</p>
+<h1 align="center">iftar</h1>
+<p align="center">
+<img src="https://img.shields.io/npm/dt/iftar.svg?maxAge=2592000">
+<img src="https://img.shields.io/npm/v/iftar.svg?maxAge=2592000">
+<img src="https://img.shields.io/npm/l/iftar.svg?maxAge=2592000">
+<img src="https://img.shields.io/badge/cli-iftar-orange.svg?maxAge=2592000">
+</p>
+
+# install
+
+```bash 
+$ [sudo] npm install -g iftar
+```
+
+# use
+
+```bash
+$ iftar <city_name>
+```
+
+# example
+
+```bash
+$ iftar istanbul
+
+$ ⏳ 20:47 İftara 3 saat 40 dakika kaldı
+```
+<p align="center">
+<a href="https://asciinema.org/a/4kl96d4dq2ocfzoagust6oj7p" target="_blank"><img src="https://asciinema.org/a/4kl96d4dq2ocfzoagust6oj7p.png" width="600" /></a>
+</p>
+
+
---
+++ index.js
@@ -0,0 +1,73 @@
+const request = require('request');
+const emoji   = require('node-emoji');
+const colors  = require('colors');
+const program = require('commander');
+const spinner = require('cli-spinner').Spinner;
+
+
+let spin = new spinner('%s Güncel veriler alınıyor.');
+spin.setSpinnerString('|/-\\');
+spin.start();
+
+program
+  .version('1.0.1')
+  .arguments('<city>')
+  .action(function (city) {
+
+    request('https://erdogan.pw/api/iftar/v1/?city=' + city, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            spin.stop(true);
+
+            // Parse
+            let jsonParse   = JSON.parse(body),
+            stringHour;
+
+            // Date to second
+            let hoursToSecond      = new Date().getHours() * 3600;
+            let minuteToSecond     = new Date().getMinutes() * 60;
+            let totalSecond        = hoursToSecond + minuteToSecond;
+
+            // Json date to second
+            let jsonDate            = jsonParse.aksam.split(':');
+
+            let jsonHourToSecond    = jsonDate[0] * 3600;
+            let jsonMinuteToSecond  = jsonDate[1] * 60;
+            let jsonTotalSecond     = jsonHourToSecond + jsonMinuteToSecond;
+
+
+            let timeDiffToMinute = (jsonTotalSecond - totalSecond) / 60;
+
+
+            let n       = Number(timeDiffToMinute);
+            let Hours   = Math.floor(n % 3600 / 60);
+            let Minutes = Math.floor(n % 3600 % 60);
+
+            if(Hours > 0 || Minutes > 0){
+              stringHour = 'İftara '+((Hours > 0 ? colors.cyan.underline(Hours) + ' saat ' : '') + (Minutes > 0 ? colors.cyan.underline(Minutes) + ' dakika ' : '') + 'kaldı');
+            }else{
+              stringHour = 'İftar vakti geçti';
+            }
+
+            console.log('');
+            console.log(emoji.get(':hourglass_flowing_sand:') + '   ' + jsonParse.aksam + ' ' + stringHour);
+            console.log('');
+        } else {
+            spin.stop(true);
+            console.log('');
+            console.error('Sunucu yanıt vermiyor');
+            console.log('');
+        }
+    })
+
+  });
+
+
+program.on('--help', function(){
+    console.log('  Örnek Kullanım:');
+    console.log('');
+    console.log('    $ iftar istanbul');
+    console.log('    $ iftar ankara');
+    console.log('');
+});
+
+program.parse(process.argv);
