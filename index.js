550b2603e680dffc7a6d4fc855972dede79bc07b
Enes Erdogan <eneserdgnn@yandex.com>
2016-07-06 22:12:58 +0300

    * 2.0.4 released.

--- index.js
+++ index.js
@@ -12,55 +12,65 @@ spin.setSpinnerString('|/-\\');
 spin.start();
 
 program
-  .version('2.0.1')
+  .version('2.0.4')
   .arguments('<city>')
   .action(function (city) {
 
-    request('https://iftar.herokuapp.com/?city=' + city, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            spin.stop(true);
+    let nextRamadan = new Date(2017,5,27);
+    let todayDate   = new Date();
 
-            // Parse
-            let jsonParse   = JSON.parse(body),
-            stringHour;
+    if(todayDate < nextRamadan){
+      spin.stop(true);
+      var daysleft = Math.floor((nextRamadan - todayDate) / (24 * 60 * 60 * 1000));
+      console.log('');
+      console.log(emoji.get(':clock1030:') + "  Ramazan'a " + daysleft + " gün kaldı");
+      console.log('');
+    }else{
+      request('https://iftar.herokuapp.com/?city=' + city, function(error, response, body) {
+          if (!error && response.statusCode == 200) {
+              spin.stop(true);
 
-            // Date to second
-            let hoursToSecond      = new Date().getHours() * 3600;
-            let minuteToSecond     = new Date().getMinutes() * 60;
-            let totalSecond        = hoursToSecond + minuteToSecond;
+              // Parse
+              let jsonParse   = JSON.parse(body),
+              stringHour;
 
-            // Json date to second
-            let jsonDate            = jsonParse.aksam.split(':');
+              // Date to second
+              let hoursToSecond      = new Date().getHours() * 3600;
+              let minuteToSecond     = new Date().getMinutes() * 60;
+              let totalSecond        = hoursToSecond + minuteToSecond;
 
-            let jsonHourToSecond    = jsonDate[0] * 3600;
-            let jsonMinuteToSecond  = jsonDate[1] * 60;
-            let jsonTotalSecond     = jsonHourToSecond + jsonMinuteToSecond;
+              // Json date to second
+              let jsonDate            = jsonParse.aksam.split(':');
+
+              let jsonHourToSecond    = jsonDate[0] * 3600;
+              let jsonMinuteToSecond  = jsonDate[1] * 60;
+              let jsonTotalSecond     = jsonHourToSecond + jsonMinuteToSecond;
 
 
-            let timeDiffToMinute = (jsonTotalSecond - totalSecond) / 60;
+              let timeDiffToMinute = (jsonTotalSecond - totalSecond) / 60;
 
 
-            let n       = Number(timeDiffToMinute);
-            let Hours   = Math.floor(n % 3600 / 60);
-            let Minutes = Math.floor(n % 3600 % 60);
+              let n       = Number(timeDiffToMinute);
+              let Hours   = Math.floor(n % 3600 / 60);
+              let Minutes = Math.floor(n % 3600 % 60);
 
-            if(Hours > 0 || Minutes > 0){
-              stringHour = 'İftara '+((Hours > 0 ? colors.cyan.underline(Hours) + ' saat ' : '') + (Minutes > 0 ? colors.cyan.underline(Minutes) + ' dakika ' : '') + 'kaldı');
-            }else{
-              stringHour = 'İftar vakti geçti';
-            }
+              if(Hours > 0 || Minutes > 0){
+                stringHour = 'İftara '+((Hours > 0 ? colors.cyan.underline(Hours) + ' saat ' : '') + (Minutes > 0 ? colors.cyan.underline(Minutes) + ' dakika ' : '') + 'kaldı');
+              }else{
+                stringHour = 'İftar vakti geçti';
+              }
 
-            console.log('');
-            console.log(emoji.get(':hourglass_flowing_sand:') + '   ' + jsonParse.aksam + ' ' + stringHour);
-            console.log('');
-        } else {
-            spin.stop(true);
-            console.log('');
-            console.error('Sunucu yanıt vermiyor');
-            console.log('');
-        }
-    })
-
+              console.log('');
+              console.log(emoji.get(':hourglass_flowing_sand:') + '   ' + jsonParse.aksam + ' ' + stringHour);
+              console.log('');
+          } else {
+              spin.stop(true);
+              console.log('');
+              console.error('Sunucu yanıt vermiyor');
+              console.log('');
+          }
+      })
+    }
   });
 
 
