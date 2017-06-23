4c995e4fee343f34c0c6bb98b275fa2b5c7c6af5
Enes Erdoğan <eneserdgnn@yandex.com>
2017-06-23 16:12:44 +0300

    * 2.1.1

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 'use strict';
-
 const Vorpal   = require('vorpal')();
 const Slugify  = require('slugify');
 const Request  = require('request');
 const Moment   = require('moment');
 const TimeDiff = require('timediff');
-const Ora      = require('ora');
-const Spinner  = new Ora({
+const Ora = require('ora');
+const Spinner = new Ora({
     text: 'Hesaplanıyor',
     color: 'yellow'
 });
@@ -21,21 +20,21 @@ Vorpal
     .action(function(args, callback) {
         Spinner.start();
         let ExistRamadan = '2017-06-24';
-        let NextRamadan  = '2018-05-16';
-        if(Moment().format('YYYY-MM-DD') <= ExistRamadan ){
+        let NextRamadan = '2018-05-16';
+        if (Moment().format('YYYY-MM-DD') <= ExistRamadan) {
             let PostURL = `http://www.namazvaktim.net/json/gunluk/${Slugify(args.city.toLowerCase())}.json`;
             Request(PostURL, function(error, response, body) {
                 if (!error && response.statusCode === 200) {
                     let PrayerTimes = JSON.parse(body);
                     let RemainingTime = TimeDiff(Moment().format('YYYY-MM-DD HH:mm:ss'), Moment().format(`YYYY-MM-DD ${PrayerTimes.namazvakitleri.zaman.vakitler.aksam}:00`));
-                    return (RemainingTime.hours > 0 || RemainingTime.minutes > 0 ?  Spinner.succeed([`İftar'a ${RemainingTime.hours} saat ${RemainingTime.minutes} dakika kaldı`]) : Spinner.fail([`Bir günü daha yedin, iyisin tabi.`]) )
+                    return (RemainingTime.hours > 0 || RemainingTime.minutes > 0 ? Spinner.succeed([`İftar'a ${RemainingTime.hours} saat ${RemainingTime.minutes} dakika kaldı`]) : Spinner.fail([`Bir günü daha yedin, iyisin tabi.`]))
                 } else {
                     Spinner.fail([`Bir şeyler ters gitti, tekrar dener misin ?`]);
                 }
             });
             callback();
-        }else{
-            let NextYear = TimeDiff(Moment().format('YYYY-MM-DD'),NextRamadan);
+        } else {
+            let NextYear = TimeDiff(Moment().format('YYYY-MM-DD'), NextRamadan);
             Spinner.succeed([`Bir sonraki Ramazan ayı ${NextYear.months} ay ${NextYear.weeks} hafta ${NextYear.days} gün sonra başlıyor. `]);
         }
     });
